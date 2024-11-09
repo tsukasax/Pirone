0cf3ab397e76963c624a07f5546ba9db9fb73eb2
Tsukasa <tsukasa@gsy.onmicrosoft.com>
2024-11-09 16:29:34 +0900

    blink

---
+++ blink/blink.c
@@ -0,0 +1,16 @@
+#include "pico/stdlib.h"
+
+#define LED_DELAY_MS 100
+
+int main()
+{
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        sleep_ms(LED_DELAY_MS);
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+        sleep_ms(LED_DELAY_MS);
+    }
+}
