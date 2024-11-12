86f305e1782c9e07eb0325ac03347b148e060cb5
Tsukasa <tsukasa@gsy.onmicrosoft.com>
2024-11-13 07:08:30 +0900

    led点滅追加

--- practice/receiver/receiver.c
+++ practice/receiver/receiver.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "hardware/clocks.h"
 #include "hardware/irq.h"
 #include "receiver.pio.h"
 
@@ -9,51 +10,66 @@
 #define RCV_CH_3 8
 #define RCV_CH_4 9
 
-uint32_t ch1w, ch2w, ch3w, ch4w;
-
-PIO pio = pio0;
+PIO pio_0 = pio0;
+PIO pio_1 = pio1; 
 uint sm_0 = 0;
 uint sm_1 = 1;
 uint sm_2 = 2;
 uint sm_3 = 3;
 
-static void irq_handler(){
-    irq_clear(PIO0_IRQ_0);
+uint32_t ch1w, ch2w, ch3w, ch4w;
+uint freq_1s = 1;           // 1Hzの点滅
+uint freq_01s = 10;         // 10Hzの点滅
+
+bool test_flag = false;
+
+
+void irq_handler(){
     pio0_hw->irq = 1;
 
-    // int t_start = time_us_32();
-    ch1w = 5000 - pio_sm_get(pio, sm_0);
-    ch2w = 5000 - pio_sm_get(pio, sm_1);
-    ch3w = 5000 - pio_sm_get(pio, sm_2);
-    ch4w = 5000 - pio_sm_get(pio, sm_3);
+    ch1w = 5000 - pio_sm_get(pio_0, sm_0);
+    ch2w = 5000 - pio_sm_get(pio_0, sm_1);
+    ch3w = 5000 - pio_sm_get(pio_0, sm_2);
+    ch4w = 5000 - pio_sm_get(pio_0, sm_3);
 
-    // printf("%d\n", ch1w);
-    // printf("ch1:%d,ch2:%d\n", ch1w, ch2w);
-    printf("ch1:%d,ch2:%d,ch3:%d,ch4:%d\n", ch1w, ch2w, ch3w, ch4w);
-    // printf("%d,%d,%d,%d\n", ch1w, ch2w, ch3w, ch4w);
-    // printf("Time = %d\n", time_us_32() - t_start);
+    printf("%d,%d,%d,%d\r\n", ch1w, ch2w, ch3w, ch4w);
 }
+
 
 int main()
 {
     stdio_init_all();
     sleep_ms(1000);
 
-    uint offset = pio_add_program(pio, &receiver_program);    
-    receiver_program_init(pio, sm_0, offset, RCV_CH_1);
-    receiver_program_init(pio, sm_1, offset, RCV_CH_2);
-    receiver_program_init(pio, sm_2, offset, RCV_CH_3);
-    receiver_program_init(pio, sm_3, offset, RCV_CH_4);
+    // プロポからの受信信号用 PIO設定
+    uint offset_0 = pio_add_program(pio_0, &receiver_program);    
+    receiver_program_init(pio_0, sm_0, offset_0, RCV_CH_1);
+    receiver_program_init(pio_0, sm_1, offset_0, RCV_CH_2);
+    receiver_program_init(pio_0, sm_2, offset_0, RCV_CH_3);
+    receiver_program_init(pio_0, sm_3, offset_0, RCV_CH_4);
 
-    // IRQハンドラの設定
+    // Pico内蔵LEDの点灯・点滅用 PIO設定
+    uint offset_1 = pio_add_program(pio_1, &blink_program);
+    blink_program_init(pio_1, sm_0, offset_1, PICO_DEFAULT_LED_PIN);
+    pio_1->txf[sm_0] = (clock_get_hz(clk_sys) / (2 * freq_1s) - 3);
+
+    // PIO0の全smを同時スタート
+    pio_enable_sm_mask_in_sync(pio_0, 15);
+
+    // PIO IRQハンドラの設定
     irq_set_exclusive_handler(PIO0_IRQ_0, irq_handler);
     irq_set_enabled(PIO0_IRQ_0, true);
     pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS;
 
     
     while (true) {
-        sleep_ms(100);
-        // printf("ch1:%d\n", ch1w);
-        // printf("ch1:%d,ch2:%d,ch3:%d,ch4:%d\n", ch1w, ch2w, ch3w, ch4w);
+        if (test_flag) {
+            pio_1->txf[sm_0] = (clock_get_hz(clk_sys) / (2 * freq_1s) - 3);
+            test_flag = false;
+        }else {
+            pio_1->txf[sm_0] = (clock_get_hz(clk_sys) / (2 * freq_01s) - 3);
+            test_flag = true;
+        }
+        sleep_ms(2000);
     }
 }
